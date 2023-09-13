e9e6df53bc12fb4e55e716e371b6f635f4133e30
mutesiwilbrod <willbrodmutesi@gmail.com>
2023-09-13 12:24:14 -0700

    variable load

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-a = 98
-"""Simple variable
-"""
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
