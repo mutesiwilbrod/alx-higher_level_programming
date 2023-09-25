49032496050c7fedebcf1dd36085798db6b8810e
mutesiwilbrod <willbrodmutesi@gmail.com>
2023-09-25 07:42:30 -0700

    magic calc

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,9 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(*args)
-        return result
-    except Exception as e:
-        print(f"Exception: {e}", file=sys.stderr)
+        return fct(*args)
+    except Exception as err:
+        print("Exception:", err, file=sys.stderr)
         return None
-
