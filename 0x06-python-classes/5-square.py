6cfcbb7fcecf703812dd2ed6a6a4881d36022ab4
mutesiwilbrod <willbrodmutesi@gmail.com>
2023-09-27 09:35:22 -0700

    debuging task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,8 +6,12 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0):
-        """Initialize size (int): the size of the square."""
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
@@ -24,14 +28,13 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the area of the square."""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
         """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
         if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
-
+            print("")
