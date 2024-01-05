1bb66ebea11f90af19ededa64ff7fc188744ba71
mutesiwilbrod <willbrodmutesi@gmail.com>
2024-01-05 10:56:28 -0800

    curl

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL to send the GET request
-url=$1
-
-# Header variable
-header="X-School-User-Id: 98"
-
-# Send GET request using curl
-response=$(curl -s -H "$header" "$url")
-
-# Display the body of the response
-echo "Hello School!"
-echo "$response"
+# header var X-HolbertonSchool-User-Id sent with value 98
+curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
 
